c2503c755b85c70bae4b26cce96dbf04186d5a1a
Gianlucca Buzo <gdmbuzo@inf.ufpel.edu.br>
2019-11-13 00:11:40 -0300

    Código atualizado

--- cache_simulator.c
+++ cache_simulator.c
@@ -1,28 +1,44 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <stdbool.h>
 
+const int MAX = 1000;
+
+/*
+nsets = numero de conjuntos
+bsize = tamanho do bloco
+assoc = nível de associatividade
+subs  = politica de substituição
+flag_ = flag de saida
+*/
 
 int main(int argc, char const *argv[]){
   
-	int nsets, bsize, assoc, flag_saida,dado,maisvariavel;
-	char *subs; 
+	int nsets, bsize, assoc, flag_saida,dados[MAX],k=0,tag,i,j;
+	char *subs,file[15]; 
 	FILE *entrada;
+	//atribuições iniciais
 	nsets = atoi (argv[1]);
 	bsize = atoi (argv[2]);
 	assoc = atoi (argv[3]);
 	subs = argv [4];
 	flag_saida = atoi (argv [5]); 
-	entrada = fopen (argv [6], "rb");
+	//copiando nome do arquivo para uma string
+	entrada = fopen(argv[6],"rb");
 	
-	int x;
-	while (true){
-		int k = fscanf (entrada, "%d", &x);
-		if (k!=1) break; //para a leitura
+	// leitura até o final do arquivo, menor que o máximo
+
+	k = fread(&dados[0],sizeof(int),MAX,entrada);
+	i = nsets/assoc;
+	j = assoc;
+
+	int matriz[i][j];
+	
+	for (int t = 0; t < k ; ++t)
+	{
+		tag = dados[t]%i;
+
 	}
-  
-  	fclose (entrada);
-  	printf ("%d", x);
+  	
 	return 0;
 }
