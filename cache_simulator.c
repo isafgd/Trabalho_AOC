4a071f2d7107aa3b889bc3c88dac3f13f1dde411
Isabela Dias <isabelafgd10@gmail.com>
2019-11-14 09:56:49 -0200

    Código atualizado(errado)

--- cache_simulator.c
+++ cache_simulator.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <conio.h>
 
 const int MAX = 1000;
 unsigned int lido;
@@ -14,7 +15,7 @@ flag_ = flag de saida
 
 int main(int argc, char const *argv[]){
   
-	int nsets, bsize, assoc, flag_saida,dados[MAX],k=0,tag,i=0,j, tam=0;
+	int nsets, bsize, assoc, flag_saida,dados[MAX],k, indice,i=0,j, tam=0, cont=0,random,hit=0,missConf=0,missCap=0,missComp=0;
 	char *subs,file[15]; 
 	FILE *entrada;
 	//atribuições iniciais
@@ -36,23 +37,49 @@ int main(int argc, char const *argv[]){
         dados[tam] = __builtin_bswap32(lido);
         tam++;
     }
-
-     for(i=0; i<tam-1; i++) {
-         printf("%u\n", dados[i]);
-     }
-
-
 	
-	 /*= nsets/assoc;*/
+	k = nsets/assoc;
 	j = assoc;
 
-	int matriz[i][j];
+	int matriz[k][j];
 	
-	for (int t = 0; t < k ; ++t)
-	{
-		tag = dados[t]%i;
-
+	for(i=0; i<= k ;i++){
+		for(int t=0;t<=j;t++){
+			matriz[i][j] = -1;
+		}
 	}
+	
+	for (int t = 0; t < tam ; ++t)
+	{
+		indice = dados[t]%k;
+		for (int m = 0; m < j; ++m)
+		{
+			if(matriz[indice][m] == dados[t]){
+			hit++;
+			break;
+			}
+		}
+		for (i=0;i<j;i++){
+			if (matriz[indice][i] == -1 || matriz[indice][i] == dados[t]){
+				if (matriz[indice][i]==-1)
+				{
+					matriz[indice][i] = dados[t];
+					missComp++;
+				}
+				matriz[indice][i] = dados[t];
+
+				break;
+			}
+			else {
+				if(cont == j){
+					}
+				}
+				else
+					cont++;
+			}
+		}
+		
+	} 
   	
 	return 0;
 }
